228193a8c7f5265b6dc147c1dd5dd009a305c166
Hristo Mitkov <37326923+ChristoMitkoff@users.noreply.github.com>
2018-03-18 08:31:49 +0200

    Add files via upload

---
+++ Elevator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= counter; i++)
+            {
+                int sumOfDigits = 0;
+                int digits = i;
+                while(digits >0)
+                {
+                    sumOfDigits += digits % 10;
+                    digits = digits / 10;
+                }
+                bool special = (sumOfDigits == 5) || (sumOfDigits == 7) || (sumOfDigits == 11);
+                Console.WriteLine("{0} -> {1}", i, special);
+            }
+                
+        }
+    }
+}
