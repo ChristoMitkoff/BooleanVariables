056ca5b46185b8e39ff18f38b3c3da9c94231c00
Hristo Mitkov <37326923+ChristoMitkoff@users.noreply.github.com>
2018-03-18 08:25:18 +0200

    Add files via upload

---
+++ BooleanVariable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class VariableInHexadecimalFormat
+    {
+        static void Main(string[] args)
+        {
+            decimal a = decimal.Parse(Console.ReadLine());
+            decimal b = decimal.Parse(Console.ReadLine());
+            bool areEqual = false;
+            decimal difference = Math.Abs(a - b);
+            if (difference > 0.000001m)
+            {
+                areEqual = true;
+            }
+            if (areEqual == true)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+    
+
+}
