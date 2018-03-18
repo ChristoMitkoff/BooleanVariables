13abd2e2781454c27abd699b4b340ac68d2af159
Hristo Mitkov <37326923+ChristoMitkoff@users.noreply.github.com>
2018-03-18 08:41:24 +0200

    Add files via upload

---
+++ ComparingFloats.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal a = decimal.Parse(Console.ReadLine());
+            decimal b = decimal.Parse(Console.ReadLine());
+            bool areEqual = false;
+            decimal difference = Math.Abs(a - b);
+            if (difference > 0.000001m)
+            {
+                areEqual = true;
+            }
+            if (areEqual == true)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
