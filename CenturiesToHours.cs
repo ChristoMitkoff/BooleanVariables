e8492cfda62eaf798b80b918884a4c691f004d75
Hristo Mitkov <37326923+ChristoMitkoff@users.noreply.github.com>
2018-03-18 08:31:07 +0200

    Add files via upload

---
+++ CenturiesToHours.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+
+namespace ConsoleApp14
+{
+    class CenturiesToHours
+    {
+        static void Main(string[] args)
+        {
+            byte centuries = byte.Parse(Console.ReadLine());
+            int years = 100 * centuries;
+            long days = (long)(365.2422 * years);
+            long hours = days * 24;
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours");
+        }
+       
+    }
+}
