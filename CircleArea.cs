08fb87f926b353c861905d4f782d820da3b97194
Hristo Mitkov <37326923+ChristoMitkoff@users.noreply.github.com>
2018-03-18 08:31:29 +0200

    Add files via upload

---
+++ CircleArea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            var circeArea = Math.PI * Math.Pow(r, 2);
+            Console.WriteLine("{0:f12}", circeArea);
+
+        }
+    }
+}
