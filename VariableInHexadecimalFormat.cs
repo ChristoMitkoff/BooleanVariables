371a4a6a2491be86dd5fbcd0036c7595903ba3d0
Hristo Mitkov <37326923+ChristoMitkoff@users.noreply.github.com>
2018-03-18 08:33:09 +0200

    Add files via upload

---
+++ VariableInHexadecimalFormat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class VariableInHexadecimalFormat
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            bool output = Convert.ToBoolean(input);
+            if (output == true)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+    
+
+}
